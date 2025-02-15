2e0771e941da426d95d1bfa85b7fd277ade92605
arthurd3 <arthur_camposl@yahoo.com>
2025-02-15 17:17:00 -0300

    ADD : CLASS

---
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/domain/user/User.java
@@ -0,0 +1,36 @@
+package com.picpaysimplificado.picpaysimplificado.domain.user;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.usertype.UserType;
+
+import java.math.BigDecimal;
+
+@Entity(name = "users")
+@Table(name= "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true)
+    private String document;
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+}
