bcc888dadac414e9b02f12205a10f93149d646fe
arthurd3 <arthur_camposl@yahoo.com>
2025-02-15 12:01:34 -0300

    StartAplication

---
+++ cardapio-spring-react/demo/demo/src/main/java/com/example/demo/CardapioSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapioSpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapioSpringReactApplication.class, args);
+	}
+
+}
